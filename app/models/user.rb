d1a6e1145698882dc44daa93b10f4a632ebd3dc9
caovanson06112001 <cao.van.son@sun-asterisk.com>
2022-07-13 14:35:46 +0700

    Chap_6

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save :downcase_email
+
+  validates :email, presence: true, length: {minium: 10, maximum: 40},
+    format: {with: VALID_EMAIL_REGEX}, unique: {case_sensitive: false}
+
+  validates :name, presence: true, length: {maximum: 10}
+
+  has_secure_password
+
+  private
+
+  def downcase_email email
+    email.downcase!
+  end
+end
