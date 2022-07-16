6facc83ccf1b4993f46481da22cc27bbd2b99eb7
caovanson06112001 <cao.van.son@sun-asterisk.com>
2022-07-17 01:18:55 +0700

    chap 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,37 @@ class User < ApplicationRecord
   before_save :downcase_email
 
   validates :email, presence: true, length: {minium: 10, maximum: 40},
-    format: {with: VALID_EMAIL_REGEX}, unique: true
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
 
   validates :name, presence: true, length: {maximum: 20}
 
   has_secure_password
+
+  attr_accessor :remember_token
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost:
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 
   private
 
