fb3e7cde19ca123077cf39966177eb6567915ce8
caovanson06112001 <cao.van.son@sun-asterisk.com>
2022-07-21 15:00:36 +0700

    chap 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
+  before_action :find_by_email, only: :create
+
   def new
     render :new
   end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = t "invalid_email_password_combination"
       render :new
@@ -17,5 +19,15 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def find_by_email
+    @user = User.find_by email: params.dig(:session, :email)&.downcase
+    return if @user
+
+    flash[:danger] = t ".controller.find_error"
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,32 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: Settings.min_pass},
     allow_nil: true
 
+  attr_accessor :remember_token
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost:
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
   private
 
   def downcase_email
