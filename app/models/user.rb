7bda58cca15b97059c63511ea2fca459a821599a
caovanson06112001 <cao.van.son@sun-asterisk.com>
2022-07-21 11:17:50 +0700

    chap 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   before_save :downcase_email
 
-  validates :email, presence: true, length: {minium: Settings.min_email, maximum: Settings.max_email},
+  validates :email, presence: true,
+    length: {minium: Settings.min_email, maximum: Settings.max_email},
     format: {with: Settings.email_regex}, uniqueness: {case_sensitive: false}
 
   validates :name, presence: true, length: {maximum: Settings.max_name}
 
   has_secure_password
 
-  validates :password, presence: true, length: { minimum: Settings.min_pass }, allow_nil: true
+  validates :password, presence: true, length: {minimum: Settings.min_pass},
+    allow_nil: true
 
   private
 
